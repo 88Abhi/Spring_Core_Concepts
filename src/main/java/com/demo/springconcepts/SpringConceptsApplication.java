ac4bef5234d3f1cca9fb78a05e31e2f414e24d19
Abhishek jat <abhishekjat80094@gmail.com>
2025-02-24 00:34:05 +0530

    Initial Commit

---
+++ src/main/java/com/demo/springconcepts/SpringConceptsApplication.java
@@ -0,0 +1,12 @@
+package com.demo.springconcepts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Main class to start Spring Boot Application
+@SpringBootApplication
+public class SpringConceptsApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(SpringConceptsApplication.class, args);
+	}
+}
